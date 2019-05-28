ecf3070541fe4c8999f001e6185c02c191c1b8b9
Davide Borficchia <davide.borficchia@studio.unibo.it>
2019-05-28 13:26:13 +0200

    implementato funzione per leggere nomi dei brand in base ad un determinato prodotto

--- src/app/components/buy-online/buy-online.component.ts
+++ src/app/components/buy-online/buy-online.component.ts
@@ -18,13 +18,41 @@ import { Injectable } from '@angular/core';
 export class BuyOnlineComponent implements OnInit {
 
   shoppingList: Map<String, Food>;
+  brandsOntology: string;
+  rowBrandsOntology: string[];
+  
 
   constructor(private ds:DataService, private router:Router, private http: HttpClient) {
     this.shoppingList = ds.getShoppingList();
 
     this.http.get('./assets/brands.rdf', {responseType: 'text'})
-        .subscribe(data => console.log(data));
+        .subscribe(data => {
+          //todo leggere singole righe
+          //console.log(data)
+          this.brandsOntology = data;
+          this.rowBrandsOntology = this.brandsOntology.split("\n");    
+          this.getBrandsByProduct("Spaghetti al pomodoro");
+        });        
   }
+
+getBrandsByProduct(productName:string){
+  for (var i: number = 0; i < this.rowBrandsOntology.length; i++){
+    //console.log(this.rowBrandsOntology[i]);
+    if (this.rowBrandsOntology[i].includes(productName)){
+      console.log("------------ INDEX ------------- " + i);
+      for (var brandIndex: number = i; brandIndex > 0; brandIndex--){
+        if (this.rowBrandsOntology[brandIndex].includes("brand:name")){
+          this.getProperyValue(this.rowBrandsOntology[brandIndex]);
+          brandIndex = 0;
+        }
+      }
+    }
+  }
+}
+
+getProperyValue(row: string){
+  console.log(row.split(/(?=<)|(?<=>)/)[2]);
+}
 
   ngOnInit() {
 
